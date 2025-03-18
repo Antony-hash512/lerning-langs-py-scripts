70dc76603df0c717dba567cdeec9ae368c9257c8
Antony <antony.ictr@gmail.com>
2025-03-19 00:19:33 +0400

    import functions

--- repeat_words_checker.py
+++ repeat_words_checker.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# Импортируем только нужные функции
+from strings_deleter import create_backup, delete_lines
+
 def find_duplicate_lines(filename):
     line_count = {}
     duplicates = {}
@@ -34,6 +37,24 @@ def main():
         for line, nums in duplicates.items():
             num_str = ', '.join(map(str, nums))
             print(f"'{line}' на строках: {num_str}")
+        # спросить у пользователя, хочет ли он удалить эти строки
+        answer = input("Хотите удалить эти строки? (y/n): ")
+        if answer == 'y':
+            backup_filename = create_backup(filename)
+            # Для каждой группы дубликатов оставляем только первую строку
+            lines_to_delete = []
+            for line, line_numbers in duplicates.items():
+                # Оставляем первую строку, удаляем остальные
+                lines_to_delete.extend(line_numbers[1:])
+            
+            # Сортируем номера строк для удаления
+            lines_to_delete.sort()
+            
+            if lines_to_delete:
+                delete_lines(filename, lines_to_delete)
+                print(f"Удалены дубликаты. Оставлен один экземпляр каждого слова.")
+            else:
+                print("Нет строк для удаления.")
     else:
         print("Повторяющиеся строки не найдены.")
 
